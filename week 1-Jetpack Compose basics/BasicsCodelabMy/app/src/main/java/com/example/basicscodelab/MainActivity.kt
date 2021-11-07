c7657ba1b10d2e819c12bb9c8eb562f0efa33802
serensis <serensis@gmail.com>
2021-11-08 00:14:41 +0900

    13. Finishing touches 완료

--- week 1-Jetpack Compose basics/BasicsCodelabMy/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelabMy/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,10 +13,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,8 +53,58 @@ fun Myapp() {
 @Composable
 private fun Greetings(names: List<String> = List(1000) { "This $it" } ) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { it ->
-            Greeting(name = it)
+        items(items = names) { nm ->
+            Greeting2(name = nm)
+        }
+    }
+}
+
+@Composable
+private fun Greeting2(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+fun CardContent(name: String) {
+    val expanded = rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Row (
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(modifier = Modifier.padding(12.dp).weight(1f)) {
+            Text(text = "Hello, ")
+            Text(text = "$name!", style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.ExtraBold).copy(fontStyle = FontStyle.Italic))
+            if (expanded.value) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded.value = !expanded.value }) {
+            Icon(
+                imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded.value) {
+                    stringResource(R.string.expand_less)
+                } else {
+                    stringResource(R.string.expand_more)
+                }
+
+            )
         }
     }
 }
@@ -70,19 +127,23 @@ private fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello, ")
                 Text(text = "$name!", style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.ExtraBold).copy(fontStyle = FontStyle.Italic))
             }
-            OutlinedButton(
-                onClick = {
-                    expanded.value = !expanded.value
-                }
+            IconButton(onClick = {
+                expanded.value = !expanded.value
+            }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Icon(
+                    imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = (if (expanded.value) R.string.expand_less else R.string.expand_more).toString()
+                )
             }
         }
     }
@@ -115,6 +176,12 @@ fun OnboardingPreview() {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
